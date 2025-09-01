c588fca5404b4fbb76c6e0f712d001f6c58f3552
Marwa-Abdullah <marwabdullah000@gmail.com>
2025-09-01 17:16:50 +0300

    add tasks in js

--- Js/javascriptArray.js
+++ Js/javascriptArray.js
@@ -4,6 +4,7 @@ for (let i = 0; i < arr.length; i++) {
   console.log(`arr[${i}], elements :` + arr[i]);
 } */
 
+/* 
 var help = 2;
 
 const Sami = [59, 58, 70, 60];
@@ -27,4 +28,121 @@ function result(marks) {
 
 console.log("Sami is :" + result(Sami));
 console.log("Mohammad is :" + result(Mohammad));
-console.log("Ghufran is :" + result(Ghufran));
+console.log("Ghufran is :" + result(Ghufran)); */
+
+function prime(n) {
+  for (let j = 2; j < n; j++) {
+    if (n % j === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function fact(m) {
+  let mul = 1;
+  for (let i = 1; i <= m; i++) {
+    mul = mul * i;
+  }
+  return mul;
+}
+/* 
+const primes = [];
+for (let i = 1; i <= 100; i++){
+  if (prime(i)) {
+     primes.push(i)
+  } 
+} */
+
+function fibonatcci(n) {
+  if (n < 0) {
+    return;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  if (n === 0) {
+    return 0;
+  }
+
+  let a = 0,
+    b = 0,
+    temp;
+  for (let i = 2; i <= n; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
+}
+
+/* 
+for (let i = 0; i < 15; i++){
+  console.log(fibonatcci(i));
+} */
+
+/* function perfectNumber(n) {
+  let sum = 0;
+  for (let j = 1; j < n; j++) {
+    if (n % j === 0) {
+     sum = sum + j;
+    }
+  }
+  if (sum === n) {
+    return true;
+  }
+  else { return false; }
+}
+
+for (let i = 1; i <= 1000; i++) {
+  if (perfectNumber(i)) {
+     console.log(i);
+  }
+}
+ */
+
+function reverce(num) {
+  let result = 0;
+  while (num > 0) {
+    let remain = num % 10;
+    result = result * 10 + remain;
+    num = Math.floor(num / 10);
+  }
+  return result;
+}
+
+function isplaindrom(number) {
+  if (number == reverce(number)) {
+    return "is PlaindRom";
+  } else {
+    return "is Not PlaindRom";
+  }
+}
+
+function movezerostoend(arr) {
+  let count = 0;
+  let arr2 = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      count++;
+    } else {
+      arr2.push(arr[i]);
+    }
+  }
+
+  for (let i = arr2.length; i <= count + arr2.length; i++) {
+    arr2[i] = 0;
+  }
+
+  return arr2;
+}
+/****************************************************** */
+
+console.log(isplaindrom(12221));
+
+/****************************************************** */
+
+const u = [10, 0, 5, 20, 0, 12, 0];
+const m = movezerostoend(u);
+console.log(m);
